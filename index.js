caa14a03b0608253bc8c4607134ad4d245f5859b
Maverick Tesini <mavericktesini@gmail.com>
2017-11-09 21:06:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 
 function deleteFromDriverByKey(driver, key) {
   const newdriver = {...driver};
-  
+  delete newdriver.key;
+  delete newdriver.value;
   return newdriver;
 }
